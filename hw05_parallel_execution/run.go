5be726db334a72e6d2b4b53bcc61184e62535008
Lidiya Ribakova <lida.ribakova@gmail.com>
2021-09-29 17:06:50 +0300

    hw05_parallel_execution working v1 is done

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -8,8 +8,58 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-func Run(tasks []Task, n, m int) error {
-	// Place your code here.
-	return nil
+func Run(tasks []Task, n, m int) (resultErr error) {
+	tasksCh := make(chan Task, n)
+	lastTaskID := n
+	for i := 0; i < lastTaskID; i++ {
+		tasksCh <- tasks[i]
+	}
+
+	errorsCh := make(chan error)
+	terminatedCh := make(chan struct{})
+
+	for i := 0; i < n; i++ {
+		go doTask(tasksCh, terminatedCh, errorsCh)
+	}
+
+	for {
+		select {
+		case err := <-errorsCh:
+			if m < 0 || lastTaskID > len(tasks) {
+				break
+			}
+			if err != nil {
+				m--
+				if m == 0 {
+					resultErr = ErrErrorsLimitExceeded
+					close(tasksCh)
+				}
+			}
+			if lastTaskID != len(tasks) && resultErr == nil {
+				tasksCh <- tasks[lastTaskID]
+				lastTaskID++
+				break
+			}
+			if lastTaskID == len(tasks) && resultErr == nil {
+				lastTaskID++
+				close(tasksCh)
+			}
+		case <-terminatedCh:
+			n--
+			if n == 0 {
+				return resultErr
+			}
+		}
+	}
+}
+
+func doTask(tasksCh chan Task, terminatedCh chan struct{}, errorsCh chan error) {
+	for {
+		task, ok := <-tasksCh
+		if !ok {
+			terminatedCh <- struct{}{}
+			return
+		}
+		errorsCh <- task()
+	}
 }
